4718ba34d18887aeea6f5d940a3f73e6ff967466
goveo <yuriybrusentsov@gmail.com>
2021-09-30 01:00:08 +0300

    Fixed database connection

--- backend/src/database/lib/connection.ts
+++ backend/src/database/lib/connection.ts
@@ -1,5 +1,13 @@
 import { Sequelize } from 'sequelize';
 import { DATABASE_URL } from '../../app.config';
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default sequelize;
